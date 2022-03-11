6058c7fd1eaa2d3034c3d39213569ffb25f793f5
Denis Vasilev <dvvasilyev@gmail.com>
2022-03-11 09:11:12 +0300

    coredata and unit testing are next steps

--- sources/apps/sweaty/sources/ContentView.swift
+++ sources/apps/sweaty/sources/ContentView.swift
@@ -4,12 +4,27 @@ import Logging
 struct ContentView: View {
     
     @State var isChecked:Bool = true
-    @State var isNotChecked:Bool = true
-    func toggle(){isChecked = !isChecked}
+    func toggle(){
+//        isChecked = !isChecked
+    }
     
     var body: some View {
         VStack {
             Text(SweatyStrings.hiMessage("Tuist + SwiftGen"))
+
+            Button(action: toggle){
+                HStack{
+                    Image(systemName: !isChecked ? "checkmark.square": "square")
+                    Text("unit test target")
+                }
+            }
+
+            Button(action: toggle){
+                HStack{
+                    Image(systemName: !isChecked ? "checkmark.square": "square")
+                    Text("coredata model")
+                }
+            }
 
             Button(action: toggle){
                 HStack{
